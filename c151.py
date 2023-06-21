c8f6edecf40f5faecf8d27411eaec79af42edc40
dumblydoor <96972278+dumblydoor@users.noreply.github.com>
2023-06-21 17:29:50 +1000

    Add files via upload

--- c151.py
+++ c151.py
@@ -6,9 +6,14 @@ Created on Sat Jun 17 16:57:55 2023
 @author: aquilavijayanayagam
 """
 
+from tkinter import*
+import random
+root=Tk()
+root.title("profits")
+root.geometry("400x400")
 month =("jan","feb"",mar","apr","may","jun","jul","aug","sep","oct","nov","dec")
 
-profits =("10000","1000000","10000000","1000000000","100000000000","10000000000000","100000000000000","1000000000000","1000000000000000","10000000000000","10000000000","10000000000000000.1")
+profits =("20","15","20","32","52","16","1","0.5","12","13","88","99.1")
 
 max_profits = max(profits)
 max_profits_index=max_profits.index(max_profits)
@@ -27,5 +32,15 @@ max_profits_months = max(month)[max_profits_index]
 print("the minimum profit was " + str(min_profits) + " it was recorded in " + str(max_profits_months))
 
 
+label1=Label(root,text=month)
+label2=Label(root,text=profits)
+button1=Button(root, text="show most profitable month",command=print("the minimum profit was " + str(min_profits) + " it was recorded in " + str(max_profits_months)))
+button2=Button(root,text="show least profitable month",command=print("the minimum profit was " + str(min_profits) + " it was recorded in " + str(max_profits_months)))
+
+label1.place(relx= 0.5, rely=0.2, anchor = CENTER)
+label2.place(relx= 0.5, rely=0.3, anchor = CENTER)
+button1.place(relx= 0.5, rely=0.5, anchor = CENTER)
+button2.place(relx= 0.5, rely=0.6, anchor = CENTER)
+root.mainloop()
 
 
